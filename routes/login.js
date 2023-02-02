c1f5462e68454b0aa69fee4428a83d79ee36678e
bchooxg <56596358+bchooxg@users.noreply.github.com>
2023-02-02 12:12:35 +0800

    Added validation so that only admin users can enter the web portal

--- routes/login.js
+++ routes/login.js
@@ -57,6 +57,11 @@ router.post("/", (req, res) => {
           if (fromApp) {
             return res.status(200).json(user);
           }
+          cons
+          if(!user.is_admin){
+              return res.status(401).send("User is not authorized to login to the web portal");
+          }
+
           logAction("Login", username);
 					resetLoginAttempts(username);
           // Create a token
